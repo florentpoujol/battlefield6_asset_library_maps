98df527f591ebdc1d28a4bd2e86197e7a971114a
Florent Poujol <florentpoujol@fastmail.com>
2025-10-26 17:07:48 +0100

    add files for Firestorm

--- portal/mp_firestorm_asset_library.ts
+++ portal/mp_firestorm_asset_library.ts
@@ -1,4 +1,11 @@
 
+/**
+ * Script for the "mp_firestorm_asset_library" map
+ * Build by Florent Poujol
+ * Sources: https://github.com/florentpoujol/battlefield6_asset_library_maps
+ * Built on: Sun Oct 26 17:04:36     2025
+ */
+
 export class SpawnedObject {
     constructor(
         public readonly name: string,
@@ -178,4 +185,81 @@ export class ObjectSpawner
             mod.ZComponentOf(closestObject.position)
         ));
     }
+}
+
+// import {ObjectSpawner} from "./common";
+
+let objectSpawner: ObjectSpawner;
+let player: mod.Player|undefined;
+
+export async function OnGameModeStarted(): Promise<void>
+{
+    objectSpawner = new ObjectSpawner(
+        mod.RuntimeSpawn_FireStorm,
+        [],
+        250.3,
+        10,
+        [
+            'GasTank_01', // 101
+            'PressureTank_02',
+            'CliffLarge_03',
+            'CliffLarge_04',
+            'CliffHuge_08B',
+            'CliffHuge_08B_B',
+            'CliffHuge_10_C90',
+            'ConstructionSite_03_C',
+            'ConstructionSite_04_B',
+            'ConstructionSite_04_PropsA', // 110
+            'ConstructionSite_04_PropsB',
+            'ConstructionSite_04_PropsC',
+            'RefineryConstruction_01',
+            'RefineryBridge_01',
+            'RefineryBridge_02',
+            'FXSiloMedium_01',
+            'FXSiloOilHuge_02',
+            'SiloMedium_01',
+            'SiloOilHuge_01',
+            'SiloOilHuge_02', // 120
+            'OilChimneyPlatform_02',
+            'RefineryTowerPlatform_01_A',
+            'RefineryTowerPlatform_01_B',
+            'RefineryTowerPlatform_01_Beams',
+            'Warehouse_01_A',
+            'Warehouse_01_B',
+            'Warehouse_01_C',
+            'Warehouse_01_D',
+            'ConstructionSite_02_B',
+            'StorageShed_01', // 130
+            'SiloOilHugeRoof_02',
+            'Maze_02',
+            'Maze_03',
+        ],
+        101
+    );
+
+    objectSpawner.spawnObjects();
+
+    while (true) {
+        await mod.Wait(1);
+
+        if (!player) {
+            await mod.Wait(5);
+            continue;
+        }
+
+        objectSpawner.OnUpdate(player);
+    }
+}
+
+export function OnPlayerDeployed(eventPlayer: mod.Player): void
+{
+    player = eventPlayer;
+    objectSpawner.createUI(eventPlayer);
+    mod.SetPlayerMovementSpeedMultiplier(player, 2);
+}
+
+export function OnPlayerUndeploy(): void
+{
+    player = undefined;
+    objectSpawner.destroyUI();
 }
